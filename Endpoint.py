a56e3b81ed5d8afdbcf80aaba9b236c807556d60
Agustin Echevarria <58480817+TheShadownReaven@users.noreply.github.com>
2022-04-13 17:13:22 -0300

    Update Endpoint.py

--- Endpoint.py
+++ Endpoint.py
@@ -5,7 +5,7 @@ import requests
 from random import randint
 
 api_token = 'b3a9f284e77aa2ac135c755fa6dab100' #Your api key
-client_token = '' #Your Client token
+client_token = 'eab6bad1a899972bd2bbde194ac11404b3c4bd32efc6f73dd19d07ccd382d8c8' #Your Client token
 def SplitDataClient(data):
     d = data.split('\n')
     request = d[0].split(' ')
